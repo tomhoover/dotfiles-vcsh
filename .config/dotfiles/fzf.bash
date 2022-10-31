e8f9fe813bb7028e7cb3482dff4fb766630d27da
Tom Hoover <tom@hisword.net>
2022-10-31 17:51:47 -0500

    Update fzf.bash & fzf.zsh for M1 Macs

--- .config/dotfiles/fzf.bash
+++ .config/dotfiles/fzf.bash
@@ -1,13 +1,29 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ -e /opt/homebrew/opt/fzf/bin/fzf ]]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+      PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+else
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.bash"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
--- .config/dotfiles/fzf.zsh
+++ .config/dotfiles/fzf.zsh
@@ -1,13 +1,29 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ -e /opt/homebrew/opt/fzf/bin/fzf ]]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+      PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+else
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    [ -r /usr/local/opt/fzf/shell/key-bindings.zsh ] && source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-[ -r /usr/local/opt/fzf/shell/key-bindings.zsh ] && source "/usr/local/opt/fzf/shell/key-bindings.zsh"
